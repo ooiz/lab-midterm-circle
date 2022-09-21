459d8a95d940beda6681b3bd03b4facf79f906f5
ooiz <project@kche.net>
2022-09-21 16:59:38 +0200

    Post page test in netlify

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-var output = document.querySelector("#project");
-function api() {
+var projectID = document.querySelector("#project");
+var postID = document.querySelector("#post");
+var newRoute = "";
+var postNum = Number(window.location.search.replace(/\W/g, ""));
+if (postID != null)
+    newRoute = "../";
+console.log(postNum);
+function homeApi() {
     fetch("https://jsonplaceholder.typicode.com/posts?_start=0&_end=3")
         .then(function (response) { return response.json(); })
         .then(function (res) {
         var apiData = "<h2>Recent Projects</h2>";
         res.forEach(function (post) {
-            apiData += "<div>\n          <img src=\"img/1.jpg\" alt=\"1.jpg\" />\n          <h3>".concat(post.title, "</h3>\n          <p>").concat(post.body, "</p>\n          <a href=\"\">Learn More</a>\n        </div>");
+            apiData += "<div>\n          <img src=\"".concat(newRoute, "img/1.jpg\" alt=\"1.jpg\" />\n          <h3>").concat(post.title, "</h3>\n          <p>").concat(post.body, "</p>\n          <a href=\"\">Learn More</a>\n        </div>");
         });
-        output.innerHTML = apiData;
+        projectID.innerHTML = apiData;
     })["catch"](function (error) { return console.log(error); });
 }
-api();
+homeApi();
+function postApi() {
+    fetch("https://jsonplaceholder.typicode.com/posts/".concat(postNum))
+        .then(function (response) { return response.json(); })
+        .then(function (res) {
+        return (postID.innerHTML = "<div>\n    <img src=\"".concat(newRoute, "img/1.jpg\" alt=\"1.jpg\" />\n    <h3>").concat(res.title, "</h3>\n    <p>").concat(res.body, "</p>\n    <a href=\"\">Learn More</a>\n  </div>"));
+    })["catch"](function (error) { return console.log(error); });
+}
+postApi();
--- script.ts
+++ script.ts
@@ -1,6 +1,12 @@
-let output = document.querySelector("#project") as HTMLDivElement;
+let projectID = document.querySelector("#project") as HTMLDivElement;
+let postID = document.querySelector("#post") as HTMLDivElement;
+let newRoute: string = "";
+let postNum: number = Number(window.location.search.replace(/\W/g, ""));
+if (postID != null) newRoute = "../";
 
-function api<T>(): void {
+console.log(postNum);
+
+function homeApi<T>(): void {
   fetch("https://jsonplaceholder.typicode.com/posts?_start=0&_end=3")
     .then((response) => response.json())
     .then((res) => {
@@ -8,15 +14,32 @@ function api<T>(): void {
 
       res.forEach((post) => {
         apiData += `<div>
-          <img src="img/1.jpg" alt="1.jpg" />
+          <img src="${newRoute}img/1.jpg" alt="1.jpg" />
           <h3>${post.title}</h3>
           <p>${post.body}</p>
           <a href="">Learn More</a>
         </div>`;
       });
-      output.innerHTML = apiData;
+      projectID.innerHTML = apiData;
     })
     .catch((error) => console.log(error));
 }
 
-api();
+homeApi();
+
+function postApi<T>(): void {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${postNum}`)
+    .then((response) => response.json())
+    .then(
+      (res) =>
+        (postID.innerHTML = `<div>
+    <img src="${newRoute}img/1.jpg" alt="1.jpg" />
+    <h3>${res.title}</h3>
+    <p>${res.body}</p>
+    <a href="">Learn More</a>
+  </div>`)
+    )
+    .catch((error) => console.log(error));
+}
+
+postApi();
