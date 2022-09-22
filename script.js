9eecda5ab0bc7f21b7191ef09ea41f6f7b28b7b9
ooiz <project@kche.net>
2022-09-22 07:15:23 +0200

    finishing javascript

--- script.js
+++ script.js
@@ -1,27 +1,78 @@
 var projectID = document.querySelector("#project");
 var postID = document.querySelector("#post");
+var form = document.querySelector("form");
 var newRoute = "";
-var postNum = Number(window.location.search.replace(/\W/g, ""));
-if (postID != null)
+var postNum = Number(window.location.search.replace(/[^1-3]/g, ""));
+if (postNum === 0)
+    postNum++;
+var apiData;
+if (postID != null) {
     newRoute = "../";
-console.log(postNum);
+    apiData = "<h2>Others Projects</h2>";
+    postApi();
+}
+if (projectID != null) {
+    apiData = "<h2>Recent Projects</h2>";
+    homeApi();
+}
+if (form != null)
+    form.addEventListener("submit", addPost);
 function homeApi() {
     fetch("https://jsonplaceholder.typicode.com/posts?_start=0&_end=3")
         .then(function (response) { return response.json(); })
         .then(function (res) {
-        var apiData = "<h2>Recent Projects</h2>";
         res.forEach(function (post) {
-            apiData += "<div>\n          <img src=\"".concat(newRoute, "img/1.jpg\" alt=\"1.jpg\" />\n          <h3>").concat(post.title, "</h3>\n          <p>").concat(post.body, "</p>\n          <a href=\"\">Learn More</a>\n        </div>");
+            apiData += "<div>\n          <img src=\"".concat(newRoute, "img/").concat(post.id, ".jpg\" alt=\"").concat(post.id, ".jpg\" />\n          <h3>").concat(post.title, "</h3>\n          <p>").concat(post.body, "</p>\n          <a href=\"post/?").concat(post.id, "\">Learn More</a>\n          </div>");
         });
         projectID.innerHTML = apiData;
     })["catch"](function (error) { return console.log(error); });
 }
-homeApi();
 function postApi() {
     fetch("https://jsonplaceholder.typicode.com/posts/".concat(postNum))
         .then(function (response) { return response.json(); })
         .then(function (res) {
-        return (postID.innerHTML = "<div>\n    <img src=\"".concat(newRoute, "img/1.jpg\" alt=\"1.jpg\" />\n    <h3>").concat(res.title, "</h3>\n    <p>").concat(res.body, "</p>\n    <a href=\"\">Learn More</a>\n  </div>"));
+        return (postID.innerHTML = "<div>\n        <h1>".concat(res.title, "</h1>\n        <img src=\"").concat(newRoute, "img/").concat(res.id, ".jpg\" alt=\"").concat(res.id, ".jpg\" />\n        <h3>").concat(res.body, "</h3>\n        </div>"));
     })["catch"](function (error) { return console.log(error); });
 }
-postApi();
+/* Contact page */
+var API_URL = "https://jsonplaceholder.typicode.com/comments";
+function addPost(preventForm) {
+    preventForm.preventDefault();
+    var contactname = form.querySelector("#contactname");
+    contactname.value;
+    var mail = form.querySelector("#mail");
+    mail.value;
+    var phone = form.querySelector("#phone");
+    phone.value;
+    var msg = form.querySelector("#msg");
+    msg.value;
+    var out = document.querySelector("#out");
+    fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json, text/plain"
+        },
+        body: JSON.stringify({
+            name: contactname,
+            email: mail,
+            phone: phone,
+            body: msg
+        })
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (dataForm) { return console.log(dataForm); })
+        .then(function () {
+        var message = "";
+        message += "\n\t\t   <div id=\"message2\">\n\t\t  <h6>Message Submitted</h6>\n\t  </div> \n\t\t  ";
+        out.innerHTML = message;
+    })["catch"](function () {
+        var message = "";
+        message += "\n\t\t\t <div id=\"message2\">\n\t\t\t<h6>oh oh, there has been an error with your message!</h6>\n\t\t</div> \n\t\t\t";
+        out.innerHTML = message;
+    });
+    contactname.value = "";
+    mail.value = "";
+    phone.value = "";
+    msg.value = "";
+}
--- script.ts
+++ script.ts
@@ -1,31 +1,37 @@
 let projectID = document.querySelector("#project") as HTMLDivElement;
 let postID = document.querySelector("#post") as HTMLDivElement;
+let form = document.querySelector("form") as HTMLFormElement;
 let newRoute: string = "";
-let postNum: number = Number(window.location.search.replace(/\W/g, ""));
-if (postID != null) newRoute = "../";
-
-console.log(postNum);
+let postNum: number = Number(window.location.search.replace(/[^1-3]/g, ""));
+if (postNum === 0) postNum++;
+let apiData: string;
+if (postID != null) {
+  newRoute = "../";
+  apiData = "<h2>Others Projects</h2>";
+  postApi();
+}
+if (projectID != null) {
+  apiData = "<h2>Recent Projects</h2>";
+  homeApi();
+}
+if (form != null) form.addEventListener("submit", addPost);
 
 function homeApi<T>(): void {
   fetch("https://jsonplaceholder.typicode.com/posts?_start=0&_end=3")
     .then((response) => response.json())
     .then((res) => {
-      let apiData = "<h2>Recent Projects</h2>";
-
       res.forEach((post) => {
         apiData += `<div>
-          <img src="${newRoute}img/1.jpg" alt="1.jpg" />
+          <img src="${newRoute}img/${post.id}.jpg" alt="${post.id}.jpg" />
           <h3>${post.title}</h3>
           <p>${post.body}</p>
-          <a href="">Learn More</a>
-        </div>`;
+          <a href="post/?${post.id}">Learn More</a>
+          </div>`;
       });
       projectID.innerHTML = apiData;
     })
     .catch((error) => console.log(error));
 }
-
-homeApi();
 
 function postApi<T>(): void {
   fetch(`https://jsonplaceholder.typicode.com/posts/${postNum}`)
@@ -33,13 +39,67 @@ function postApi<T>(): void {
     .then(
       (res) =>
         (postID.innerHTML = `<div>
-    <img src="${newRoute}img/1.jpg" alt="1.jpg" />
-    <h3>${res.title}</h3>
-    <p>${res.body}</p>
-    <a href="">Learn More</a>
-  </div>`)
+        <h1>${res.title}</h1>
+        <img src="${newRoute}img/${res.id}.jpg" alt="${res.id}.jpg" />
+        <h3>${res.body}</h3>
+        </div>`)
     )
     .catch((error) => console.log(error));
 }
 
-postApi();
+/* Contact page */
+
+const API_URL: string = "https://jsonplaceholder.typicode.com/comments";
+
+function addPost(preventForm): void {
+  preventForm.preventDefault();
+
+  let contactname = form.querySelector("#contactname") as HTMLInputElement;
+  contactname.value;
+  let mail = form.querySelector("#mail") as HTMLInputElement;
+  mail.value;
+  let phone = form.querySelector("#phone") as HTMLInputElement;
+  phone.value;
+  let msg = form.querySelector("#msg") as HTMLTextAreaElement;
+  msg.value;
+  let out = document.querySelector("#out") as HTMLDivElement;
+
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json, text/plain",
+    },
+    body: JSON.stringify({
+      name: contactname,
+      email: mail,
+      phone: phone,
+      body: msg,
+    }),
+  })
+    .then((response) => response.json())
+    .then((dataForm) => console.log(dataForm))
+    .then(() => {
+      let message = "";
+      message += `
+		   <div id="message2">
+		  <h6>Message Submitted</h6>
+	  </div> 
+		  `;
+      out.innerHTML = message;
+    })
+    .catch(() => {
+      let message = "";
+      message += `
+			 <div id="message2">
+			<h6>oh oh, there has been an error with your message!</h6>
+		</div> 
+			`;
+      out.innerHTML = message;
+    });
+
+  contactname.value = "";
+  mail.value = "";
+  phone.value = "";
+  msg.value = "";
+}
